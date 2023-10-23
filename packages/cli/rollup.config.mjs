09635570a4be6bd2554d15be1444102893a74972
José Moreira <josemoreiravarzim@gmail.com>
2023-10-23 18:24:41 +0100

    chore: make the cli.js file executable upon a successful build

--- packages/cli/rollup.config.mjs
+++ packages/cli/rollup.config.mjs
@@ -3,6 +3,7 @@ import builtins from 'rollup-plugin-node-builtins';
 import { swc, defineRollupSwcOption } from 'rollup-plugin-swc3';
 import replace from '@rollup/plugin-replace';
 import pkg from './package.json' assert { type: 'json' };
+import { chmod } from 'node:fs/promises'
 
 const input = 'src/cli.ts';
 
@@ -33,6 +34,22 @@ export default [
           },
         }),
       ),
+      makeOutputExecutablePlugin()
     ],
   },
 ];
+
+
+
+function makeOutputExecutablePlugin() {
+  return {
+        name: 'writeBundle',
+        writeBundle: {
+          sequential: true,
+          order: 'post',
+          async handler({ file}) {
+            await chmod(file, 0o755);
+          }
+        }
+      }
+}
